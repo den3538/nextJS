1b932f73cf83ce6aa2a946a5c5960076391468ec
Denys <den3538@gmail.com>
2026-03-12 01:20:29 +0100

    create sign up, sign in and init auth

---
+++ jobs-application-tracker/jobs-application-tracker/app/sign-in/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export default function SignInPage() {
+    return (
+        <div className="flex flex-1 items-center justify-center bg-white p-4">
+            <Card className="w-full max-w-md mx-auto border-gray-200 shadow-lg">
+                <CardHeader className="space-y-1">
+                    <CardTitle className="text-2xl font-bold text-black">
+                        Sign In
+                    </CardTitle>
+                    <CardDescription className="text-gray-600">
+                        Sign in to your account to continue tracking your job applications and managing your career journey effectively.
+                    </CardDescription>
+                </CardHeader>
+                <form className="space-y-4">
+                    <CardContent className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" name="email" placeholder="Your email address" type="email" required />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" name="password" placeholder="Your password" type="password" required className="border-gray-300 focus:border-primary" minLength={8}/>
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex flex-col space-y-4">
+                        <Button type="submit" className="w-full bg-primary text-white hover:bg-white hover:text-primary hover:border hover:border-primary">
+                            Sign In
+                        </Button>
+                        <p className="gap-x-10">Don&apos;t have an account? <Link href="/sign-up" className="underline">Sign Up</Link></p>
+                    </CardFooter>
+                </form>
+            </Card>
+        </div>
+    );
+}
